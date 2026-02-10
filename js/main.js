c1b00ee8888cbaabec1c5f1839e3d2bae914dad5
pashechkar-max <polinausakova921@gmail.com>
2026-02-10 17:24:25 +0700

    commit 2

--- js/main.js
+++ js/main.js
@@ -4,5 +4,54 @@ Vue.component('task-card', {
         column: String
     },
 
+    methods: {
+        editTask(){
+            this.$emit('edit', this.task);
+        }
+    },
 
+    template: `
+    <div class="card">
+        <h3>{{ task.title }}</h3>
+
+        <p>{{ task.description }}</p>
+
+        <small>
+            
+            Created: {{ task.createdAt }} <br>
+            Updated: {{ task.updatedAt }} <br>
+            Deadline: {{ task.deadline }}
+        </small>
+
+        <p v-if="task.returnReason">
+            <b>Reason for return:</b> {{ task.returnReason }}
+        </p>
+
+        <div class="actions">
+            <button @click="editTask">Edit</button>
+
+            <button 
+                v-if="column !== 'done'" 
+                @click="moveForward"
+            >
+                ->
+            </button>
+
+            <button 
+                v-if="column === 'testing'" 
+                @click="moveBack"
+            >
+                <- Return
+            </button>
+
+            <span v-if="task.isOverdue" class="overdue">
+                Overdue
+            </span>
+
+            <span v-if="task.isCompletedInTime" class="ok">
+                Completed in time
+            </span>
+        </div>
+    </div>
+    `
 })
